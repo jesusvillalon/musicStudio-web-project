bd3dfc6c46bf0c13c9136eef9532042b73f96d9d
Jesús Villalón <villalongallardo92@gmail.com>
2024-03-07 01:42:06 +0100

    updates

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Benefits } from 'src/app/shared/interfaces/benefits.interface';
 import { HomeCategories } from 'src/app/shared/interfaces/home-categories.interface';
 
 @Component({
@@ -8,42 +9,73 @@ import { HomeCategories } from 'src/app/shared/interfaces/home-categories.interf
 })
 export class HomePageComponent {
 
-public categories: HomeCategories[] = [
+public categories1: HomeCategories[] = [
   {
-    image: '../../../assets/images/categories/software.jpg',
+    image: '../../../assets/images/categories/software.png',
     categoryName: 'SOFTWARE'
   },
   {
-    image: '../../../assets/images/categories/audioInterface.webp',
+    image: '../../../assets/images/categories/audioInterface.png',
     categoryName: 'INTERFAZ DE AUDIO'
   },
   {
-    image: '../../../assets/images/categories/headphones.webp',
+    image: '../../../assets/images/categories/headphones.png',
     categoryName: 'AURICULARES'
   },
   {
-    image: '../../../assets/images/categories/monitores.jpeg',
+    image: '../../../assets/images/categories/monitores.png',
     categoryName: 'MONITORES'
-  },
+  }
+];
+public categories2: HomeCategories[] = [
   {
-    image: '../../../assets/images/categories/midi.jpeg',
+    image: '../../../assets/images/categories/midi.png',
     categoryName: 'MIDI'
   },
   {
-    image: '../../../assets/images/categories/mixer.webp',
+    image: '../../../assets/images/categories/mixer.png',
     categoryName: 'MESAS DE MEZCLAS'
   },
   {
-    image: '../../../assets/images/categories/microphone.webp',
+    image: '../../../assets/images/categories/microphone.png',
     categoryName: 'MICRÓFONOS'
   },
   {
-    image: '../../../assets/images/categories/cable.webp',
+    image: '../../../assets/images/categories/cable.png',
     categoryName: 'CABLES'
   },
 ];
 
+public benefits1: Benefits[] = [
+  {
+    image: '../../../assets/images/iconos/iconosVentajas/garantia.png',
+    title: '5 años de garantía'
+  },
+  {
+    image: '../../../assets/images/iconos/iconosVentajas/pagoSeguro.png',
+    title: 'Pago seguro'
+  },
+  {
+    image: '../../../assets/images/iconos/iconosVentajas/envio.png',
+    title: 'Envíos gratis (mín. 50€)'
+  },
 
+]
+public benefits2: Benefits[] = [
+  {
+    image: '../../../assets/images/iconos/iconosVentajas/bestEcommerce.png',
+    title: 'Ecommerce del año'
+  },
+
+  {
+    image: '../../../assets/images/iconos/iconosVentajas/soporteTecnico.png',
+    title: 'Soporte técnico'
+  },
+  {
+    image: '../../../assets/images/iconos/iconosVentajas/devoluciones.png',
+    title: '30 días de devoluciones'
+  },
+]
 
 
 }
---
+++ src/app/shared/interfaces/benefits.interface.ts
@@ -0,0 +1,4 @@
+export interface Benefits {
+  image: string;
+  title: string;
+}
